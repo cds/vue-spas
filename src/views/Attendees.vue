29a69d3a4b9f47ae59ddc5a266e6a3ccde0a6925
chandrakantsangale <c.sangale@gmail.com>
2020-08-28 05:51:19 +0530

    skip ramdom reset modules and All Done

--- src/views/Attendees.vue
+++ src/views/Attendees.vue
@@ -1,9 +1,47 @@
 <template>
     <div class="container mt-4">
+        <!--   Start-search-->
+        <div
+            class="row justify-content-center"
+        >
+            <div class="col-md-8"
+            v-if="user !== null && user.uid == userID"
+            >
+                <h1 class="font-weight-light text-center">Attendees</h1>
+
+                <div class="card bg-light mb-4">
+                    <div class="card-body text-center">
+                        <div class="input-group input-group-lg">
+                            <input
+                                type="text"
+                                placeholder="Search Attendees"
+                                class="form-control"
+                                v-model="searchQuery"
+                            />
+                            <div class="input-group-append">
+<!--                                <button-->
+<!--                                    class="btn btn-sm btn-outline-info"-->
+<!--                                    title="Pick a random attendee"-->
+<!--                                >-->
+<!--                                    <font-awesome-icon icon="random"></font-awesome-icon>-->
+<!--                                </button>-->
+<!--                                <button-->
+<!--                                    class="btn btn-sm btn-outline-info"-->
+<!--                                    title="Reset Search"-->
+<!--                                >-->
+<!--                                    <font-awesome-icon icon="undo"></font-awesome-icon>-->
+<!--                                </button>-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--   End-search-->
         <div class="row justify-content-center">
             <div
                 class="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
-                v-for="item in attendees"
+                v-for="item in filteredAttendees"
                 :key="item.id">
                 <div class="card">
                     <div class="card-body px-3 py-2 d-flex align-items-center justify-content-center">
@@ -48,12 +86,19 @@ export default {
     data: function () {
         return {
             attendees: [],
+            searchQuery: "",
             userID: this.$route.params.userID,
             meetingID: this.$route.params.meetingID
         };
     },
     components: {
         FontAwesomeIcon
+    },
+    computed: {
+        filteredAttendees: function () {
+            const dataFilter = item => item.displayName.toLowerCase().match(this.searchQuery.toLowerCase()) && true;
+            return this.attendees.filter(dataFilter);
+        }
     },
     methods: {
         toggleStar: function (attendeeID) {
